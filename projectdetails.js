bac291463d7ae046d06e920980b25ac8234b7393
Giridharan <mac@macs-MacBook-Air.local>
2024-01-09 19:46:20 +0530

    Updates

---
+++ projectdetails.js
@@ -0,0 +1,29 @@
+const project=[
+    {
+name:"KEEPER APP",
+Description:"Developed the frontend for a note-keeping web application, 'Keeper App,' using React.js. This front-end project enables users to seamlessly create, edit, and delete notes through an intuitive user interface. Leveraging React's useState for state management, the project ensures dynamic and responsive note handling. Custom styling enhances the user experience, providing an organised and visually appealing environment.This front-end project showcases expertise in React.js, emphasising component-based architecture and front-end development skills.",
+TechStack: "React.js",
+Githublink: "https://github.com/GiridharanSanthoshkumar/keeper-react-app"
+},
+{
+name:"SIMON GAME",
+
+Description: "Created a classic Simon game using HTML, CSS, and JavaScript with jQuery for interactivity. Players must follow a sequence of colors and sounds, progressively increasing in complexity",
+TechStack: "HTML, CSS, JavaScript (jQuery)",
+Githublink: "https://giridharansanthoshkumar.github.io/s-mon-Game/",
+},
+{
+name:"DRUM STICK PLAYER",
+Description: "Built a virtual drumstick player using HTML, CSS, and JavaScript with jQuery. Users can simulate playing the drums by clicking on the virtual drumsticks and producing corresponding sounds",
+TechStack:" HTML, CSS, JavaScript (jQuery)",
+Githublink: "https://giridharansanthoshkumar.github.io/drumstick/",
+},
+{
+name:"DICTIONARY",
+Description: "Developed an online Dictionary using the Free Dictionary API. This platform allows users to search for word definitions, learn pronunciation, and discover synonyms for the words they are interested in",
+TechStack: "HTML, CSS, JavaScript ,EJS, Node.js, Express.js, REST API(Free Dictionary API)",
+Githublink:"https://github.com/GiridharanSanthoshkumar/Dictionary",
+},
+
+]
+
